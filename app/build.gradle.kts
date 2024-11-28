54464cb0c8aa752657819bfdb994d30922e274a0
Bruno Gonçalves <bg.dev@BOOK-9ECCGUHNL>
2024-11-28 03:00:57 -0300

    update of firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation(libs.firebase.bom)
 }
